85ad4064de247ed24d013da1efb2b41e55243186
Dhruba Jyoti Seal <dhrubasil@gmail.com>
2020-09-01 03:39:38 +0530

    Association Rules
    Association Rules

--- Association Rules/Association Rules Book.R
+++ Association Rules/Association Rules Book.R
@@ -14,13 +14,20 @@ library(arulesViz)
 
 #Implementation on book dataset
 
-book <- read.csv(file.choose())
+book<-read.csv(file.choose()) #importing the dataset in csv format
+View(book) #the dataset has 
+class(book)
+book_trans<-as(as.matrix(book),"transactions") #since the data is in binary format, making it as transaction matrix
+inspect(book_trans[1:100]) #inspecting the lists first 100
 
-View(book)
+rules<-apriori(as.matrix(book),parameter = list(support=0.002,confidence=0.7)) #creating apriori algorithm with least support and confidence values
+inspect(rules[1:20])
+plot(rules)
 
-book_rules <- apriori(book,parameter=list(support=0.2, confidence = 0.5,minlen=5))
+rules1<-apriori(as.matrix(book),parameter = list(support=0.006,confidence=0.9)) #creating apriori algorithm with high support and confidence values
+inspect(rules1[1:20])
+plot(rules1)
 
-plot(book_rules,method = "scatterplot")
-plot(book_rules,method = "grouped")
-plot(book_rules,method = "graph")
-plot(book_rules,method = "mosaic")
+rules2<-apriori(as.matrix(book),parameter = list(support=0.001,confidence=0.1)) #creating apriori algorithm with least support and confidence values and only 10 rules produced when confidence reduced
+inspect(rules2[1:10])
+plot(rules2)
